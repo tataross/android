685712e2f6a6e2f1963164034fc4b621694af148
chaowen guo <chaowen.guo1@gmail.com>
2020-06-20 00:48:38 -0500

    Update MainActivity.java

--- src/main/java/com/main/MainActivity.java
+++ src/main/java/com/main/MainActivity.java
@@ -1,10 +1,4 @@
 package com.main;
-/*https://developer.android.com/studio/build/application-id
-And although the application ID looks like a traditional Java package name, the naming rules for the application ID are a bit more restrictive:
-
-It must have at least two segments (one or more dots).
-Each segment must start with a letter.
-All characters must be alphanumeric or an underscore [a-zA-Z0-9_].*/
 
 import android.app.Activity;
 import android.os.Bundle;
