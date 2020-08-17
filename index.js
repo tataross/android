241edaaf1dac391b70e5251dffb0857fca6c54e7
chaowen guo <chaowen.guo1@gmail.com>
2020-08-16 22:41:05 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 import squared from 'squared'
 const div = globalThis.document.createElement('div')
+div.textContent = 'fuck you'
+squared.parseDocument(div)
+squared.saveToArchive()
+squared.reset()
