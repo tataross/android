dc7372ce56a2d32eaae1c274a01b7b7576224b26
chaowen guo <chaowen.guo1@gmail.com>
2020-08-18 05:26:06 -0500

    Delete index.js

--- index.js
+++
@@ -1,5 +0,0 @@
-import squared from 'squared'
-const div = new globalThis.DOMParser().parseFromString('<div>fuck you</div>', 'text/html').body
-squared.parseDocument(div)
-squared.saveToArchive()
-squared.reset()
