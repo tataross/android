1bcf66c6f5697c858bd82f0b846a43ee600ab95e
chaowen guo <chaowen.guo1@gmail.com>
2020-08-18 11:16:56 -0500

    Update MainActivity.java

--- src/main/java/com/main/MainActivity.java
+++ src/main/java/com/main/MainActivity.java
@@ -1,11 +1,5 @@
 package com.main;
-  
-import android.content.ContentValues;  
-import android.content.Context;  
-import android.database.Cursor;  
-import android.database.sqlite.SQLiteDatabase;  
-import android.database.sqlite.SQLiteOpenHelper;  
-import android.graphics.Color;  
+ 
 import android.os.Bundle;  
 import android.app.Activity;  
 import android.view.Menu;  
