0d8b1d7dd4c810d82a492367e9f127b5b5d181cd
chaowen guo <chaowen.guo1@gmail.com>
2020-08-18 11:23:38 -0500

    Update MainActivity.java

--- src/main/java/com/main/MainActivity.java
+++ src/main/java/com/main/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends Activity
     public void onCreate(final Bundle savedInstanceState)   
     {
         super.onCreate(savedInstanceState);  
-        Button relativeLayout = new RelativeLayout(this);  
+        RelativeLayout relativeLayout = new RelativeLayout(this);  
         RelativeLayout.LayoutParams relativeLayoutParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.FILL_PARENT, RelativeLayout.LayoutParams.FILL_PARENT);  
         Button button1 = new Button(this);  
         button1.setText("Button1");  
