82441b40962ba59ea6f382345c0fc02c8876baf7
chaowen guo <chaowen.guo1@gmail.com>
2020-08-18 12:25:10 -0500

    Update MainActivity.java

--- src/main/java/com/main/MainActivity.java
+++ src/main/java/com/main/MainActivity.java
@@ -13,13 +13,13 @@ public class MainActivity extends Activity
         super.onCreate(savedInstanceState);  
         var relativeLayout = new RelativeLayout(this);  
         RelativeLayout.LayoutParams relativeLayoutParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.MATCH_PARENT);  
-        Button button1 = new Button(this);  
+        var button1 = new Button(this);  
         button1.setText("Button1");  
-        Button button2 = new Button(this);  
+        var button2 = new Button(this);  
         button2.setText("Button2");  
-        Button button3 = new Button(this);  
+        var button3 = new Button(this);  
         button3.setText("Button3");  
-        Button button4 = new Button(this);  
+        var button4 = new Button(this);  
         button4.setText("Button4");  
         AddButtonLayout(button1, RelativeLayout.ALIGN_PARENT_LEFT);  
         AddButtonLayout(button3, RelativeLayout.CENTER_IN_PARENT);  
@@ -29,11 +29,11 @@ public class MainActivity extends Activity
         relativeLayout.addView(button3);  
         relativeLayout.addView(button4);  
         relativeLayout.addView(button2);  
-        setContentView(relativeLayout, relativeLayoutParams);
+        super.setContentView(relativeLayout, relativeLayoutParams);
     }  
     private void LayoutAddButton(Button button, int centerInParent, int marginLeft, int marginTop, int marginRight, int marginBottom)   
     {
-        RelativeLayout.LayoutParams buttonLayoutParameters = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);  
+        var buttonLayoutParameters = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);  
         buttonLayoutParameters.setMargins(marginLeft, marginTop, marginRight, marginBottom);  
         buttonLayoutParameters.addRule(centerInParent);  
         button.setLayoutParams(buttonLayoutParameters);
