cd86f559f89d1c2a62b667356dfe0194cc089809
chaowen guo <chaowen.guo1@gmail.com>
2020-10-24 07:00:16 -0500

    Update MainActivity.java

--- src/main/java/com/main/MainActivity.java
+++ src/main/java/com/main/MainActivity.java
@@ -10,13 +10,12 @@ public class MainActivity extends android.app.Activity
     {
         super.onCreate(savedInstanceState);  
         final var relativeLayout = new RelativeLayout(this);
+        final var button1 = new Button(this);
         try (final var reader = new java.io.BufferedReader(new java.io.InputStreamReader(super.getAssets().open("file.txt"), java.nio.charset.StandardCharsets.UTF_8)))
         {
-            text.setText(reader.lines().collect(java.util.stream.Collectors.joining("\n")));
+            button1.setText(reader.lines().collect(java.util.stream.Collectors.joining("\n")));
         }
-        catch (Exception error){}
-        final var button1 = new Button(this);  
-        button1.setText("Button1");   
+        catch (Exception error){}          
         relativeLayout.addView(button1);  
         super.setContentView(relativeLayout, new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.MATCH_PARENT));
     }  
