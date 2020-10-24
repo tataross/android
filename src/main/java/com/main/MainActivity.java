92e47ef345c8f2484401f07e3e7c5e1cb4acbf90
chaowen guo <chaowen.guo1@gmail.com>
2020-10-24 05:25:44 -0500

    Update MainActivity.java

--- src/main/java/com/main/MainActivity.java
+++ src/main/java/com/main/MainActivity.java
@@ -11,8 +11,8 @@ public class MainActivity extends android.app.Activity
         super.onCreate(savedInstanceState);  
         final var body = new ConstraintLayout(this);
         final var text = new TextView(this);
-        text.setText("fuck you");
+        text.setText(new BufferedReader(new InputStreamReader(super.getAsserts().open("file.txt"), StandardCharsets.UTF_8))).lines().collect(Collectors.joining("\n")));
         body.addView(text);  
         super.setContentView(body, new ConstraintLayout.LayoutParams(ConstraintLayout.LayoutParams.MATCH_PARENT, ConstraintLayout.LayoutParams.MATCH_PARENT));
-    }
+    }                
 }
