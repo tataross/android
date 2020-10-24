9c2cef40296bc202684393fa5253f0b22549605e
chaowen guo <chaowen.guo1@gmail.com>
2020-10-24 05:28:21 -0500

    Update MainActivity.java

--- src/main/java/com/main/MainActivity.java
+++ src/main/java/com/main/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends android.app.Activity
         super.onCreate(savedInstanceState);  
         final var body = new ConstraintLayout(this);
         final var text = new TextView(this);
-        text.setText(new BufferedReader(new java.io.InputStreamReader(super.getAsserts().open("file.txt"), java.nio.charset.StandardCharset.UTF_8))).lines().collect(Collectors.joining("\n")));
+        text.setText(new java.io.BufferedReader(new java.io.InputStreamReader(super.getAsserts().open("file.txt"), java.nio.charset.StandardCharset.UTF_8))).lines().collect(Collectors.joining("\n")));
         body.addView(text);  
         super.setContentView(body, new ConstraintLayout.LayoutParams(ConstraintLayout.LayoutParams.MATCH_PARENT, ConstraintLayout.LayoutParams.MATCH_PARENT));
     }                
