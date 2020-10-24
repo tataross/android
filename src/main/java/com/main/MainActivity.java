fe9f0078602c0ce6478794269349608ad9d38c43
chaowen guo <chaowen.guo1@gmail.com>
2020-10-24 05:51:52 -0500

    Update MainActivity.java

--- src/main/java/com/main/MainActivity.java
+++ src/main/java/com/main/MainActivity.java
@@ -6,12 +6,16 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 public class MainActivity extends android.app.Activity   
 {
     @Override  
-    public void onCreate(final android.os.Bundle savedInstanceState) throws Exception
+    public void onCreate(final android.os.Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);  
         final var body = new ConstraintLayout(this);
         final var text = new TextView(this);
-        text.setText(new java.io.BufferedReader(new java.io.InputStreamReader(super.getAssets().open("file.txt"), java.nio.charset.StandardCharsets.UTF_8)).lines().collect(java.util.stream.Collectors.joining("\n")));
+        try
+        {
+            text.setText(new java.io.BufferedReader(new java.io.InputStreamReader(super.getAssets().open("file.txt"), java.nio.charset.StandardCharsets.UTF_8)).lines().collect(java.util.stream.Collectors.joining("\n")));
+        }
+        catch (Exception error){}
         body.addView(text);  
         super.setContentView(body, new ConstraintLayout.LayoutParams(ConstraintLayout.LayoutParams.MATCH_PARENT, ConstraintLayout.LayoutParams.MATCH_PARENT));
     }                
