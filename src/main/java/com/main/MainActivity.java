33bfc47fdd438c30a093f496cf58178ed1d8b3bb
chaowen guo <chaowen.guo1@gmail.com>
2020-10-24 06:07:07 -0500

    Update MainActivity.java

--- src/main/java/com/main/MainActivity.java
+++ src/main/java/com/main/MainActivity.java
@@ -1,22 +1,43 @@
 package com.main;
  
-import android.widget.TextView;  
-import androidx.constraintlayout.widget.ConstraintLayout;
+import android.widget.Button;  
+import android.widget.RelativeLayout;
 
 public class MainActivity extends android.app.Activity   
 {
     @Override  
-    public void onCreate(final android.os.Bundle savedInstanceState)
+    public void onCreate(final android.os.Bundle savedInstanceState)   
     {
         super.onCreate(savedInstanceState);  
-        final var body = new ConstraintLayout(this);
-        final var text = new TextView(this);
-        try (final var reader = new java.io.BufferedReader(new java.io.InputStreamReader(super.getAssets().open("file.txt"), java.nio.charset.StandardCharsets.UTF_8)))
-        {
-            text.setText(reader.lines().collect(java.util.stream.Collectors.joining("\n")));
-        }
-        catch (Exception error){}
-        body.addView(text);  
-        super.setContentView(body, new ConstraintLayout.LayoutParams(ConstraintLayout.LayoutParams.MATCH_PARENT, ConstraintLayout.LayoutParams.MATCH_PARENT));
-    }                
+        var relativeLayout = new RelativeLayout(this);  
+        var relativeLayoutParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.MATCH_PARENT);  
+        var button1 = new Button(this);  
+        button1.setText("Button1");  
+        var button2 = new Button(this);  
+        button2.setText("Button2");  
+        var button3 = new Button(this);  
+        button3.setText("Button3");  
+        var button4 = new Button(this);  
+        button4.setText("Button4");  
+        AddButtonLayout(button1, RelativeLayout.ALIGN_PARENT_LEFT);  
+        AddButtonLayout(button3, RelativeLayout.CENTER_IN_PARENT);  
+        AddButtonLayout(button4, RelativeLayout.ALIGN_PARENT_BOTTOM);  
+        LayoutAddButton(button2, RelativeLayout.ALIGN_PARENT_LEFT, 30, 80, 0, 0);  
+        relativeLayout.addView(button1);  
+        relativeLayout.addView(button3);  
+        relativeLayout.addView(button4);  
+        relativeLayout.addView(button2);  
+        super.setContentView(relativeLayout, relativeLayoutParams);
+    }  
+    private void LayoutAddButton(Button button, int centerInParent, int marginLeft, int marginTop, int marginRight, int marginBottom)   
+    {
+        var buttonLayoutParameters = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);  
+        buttonLayoutParameters.setMargins(marginLeft, marginTop, marginRight, marginBottom);  
+        buttonLayoutParameters.addRule(centerInParent);  
+        button.setLayoutParams(buttonLayoutParameters);
+    }  
+    private void AddButtonLayout(Button button, int centerInParent)   
+    {  
+        LayoutAddButton(button, centerInParent, 0, 0, 0, 0);  
+    }  
 }
