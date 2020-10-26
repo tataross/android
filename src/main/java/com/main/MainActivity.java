7319b811aea151b3738ae8d2039fb9467209a9ef
chaowen guo <chaowen.guo1@gmail.com>
2020-10-25 23:31:18 -0500

    Update MainActivity.java

--- src/main/java/com/main/MainActivity.java
+++ src/main/java/com/main/MainActivity.java
@@ -1,25 +1,33 @@
 package com.main;
- 
-import android.widget.Button;  
+  
 import androidx.constraintlayout.widget.ConstraintLayout;
 
 public class MainActivity extends android.app.Activity   
 {
+    private android.view.View parse(final org.jsoup.select.Element element)
+    {
+        if (element.childrenSize() == 0)
+        {
+            final var button = new android.widget.Button(this);
+            Button.setText(element.text());
+            return button;
+        }
+        else
+        {
+            final var body = new ConstraintLayout(this);
+            for (final var $: document.body().children()) body.addView(this.parse($));
+            return body;
+        }
+    }
     @Override  
     public void onCreate(final android.os.Bundle savedInstanceState)   
     {
         super.onCreate(savedInstanceState);  
-        final var body = new ConstraintLayout(this);
-        final var a = new StringBuilder();
         try (final var reader = new java.io.BufferedReader(new java.io.InputStreamReader(super.getAssets().open("index.html"), java.nio.charset.StandardCharsets.UTF_8)))
         {
             final var document = org.jsoup.Jsoup.parse(reader.lines().collect(java.util.stream.Collectors.joining("\n")));
-            for (final var $: document.body().children()) a.append($.text());
+            super.setContentView(this.parse(document.body()), new ConstraintLayout.LayoutParams(ConstraintLayout.LayoutParams.MATCH_PARENT, ConstraintLayout.LayoutParams.MATCH_PARENT));
         }
-        catch (Exception error){}
-        final var button = new Button(this);
-        button.setText(a.toString());
-        body.addView(button);  
-        super.setContentView(body, new ConstraintLayout.LayoutParams(ConstraintLayout.LayoutParams.MATCH_PARENT, ConstraintLayout.LayoutParams.MATCH_PARENT));
+        catch (Exception error){} 
     }  
 }
