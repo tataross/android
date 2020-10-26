7c1e2390dccbebe7ee6f422aa0290f5b0f743826
chaowen guo <chaowen.guo1@gmail.com>
2020-10-25 22:16:32 -0500

    Update MainActivity.java

--- src/main/java/com/main/MainActivity.java
+++ src/main/java/com/main/MainActivity.java
@@ -10,13 +10,15 @@ public class MainActivity extends android.app.Activity
     {
         super.onCreate(savedInstanceState);  
         final var body = new ConstraintLayout(this);
-        final var button = new Button(this);
+        StringBuilder a;
         try (final var reader = new java.io.BufferedReader(new java.io.InputStreamReader(super.getAssets().open("index.html"), java.nio.charset.StandardCharsets.UTF_8)))
         {
             final var document = org.jsoup.Jsoup.parse(reader.lines().collect(java.util.stream.Collectors.joining("\n")));
-            button.setText(document.body().html());
+            for (final var $: document.body().children()) a.append($.text());
         }
-        catch (Exception error) {}
+        catch (Exception error){}
+        final var button = new Button(this);
+        button.setText(a.toString());
         body.addView(button);  
         super.setContentView(body, new ConstraintLayout.LayoutParams(ConstraintLayout.LayoutParams.MATCH_PARENT, ConstraintLayout.LayoutParams.MATCH_PARENT));
     }  
