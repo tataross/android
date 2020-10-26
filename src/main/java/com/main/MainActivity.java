d38d48b1e58d0f05ee761b0f0d78ce2fc51e285b
chaowen guo <chaowen.guo1@gmail.com>
2020-10-25 22:19:32 -0500

    Update MainActivity.java

--- src/main/java/com/main/MainActivity.java
+++ src/main/java/com/main/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends android.app.Activity
     {
         super.onCreate(savedInstanceState);  
         final var body = new ConstraintLayout(this);
-        StringBuilder a;
+        final var a = new StringBuilder();
         try (final var reader = new java.io.BufferedReader(new java.io.InputStreamReader(super.getAssets().open("index.html"), java.nio.charset.StandardCharsets.UTF_8)))
         {
             final var document = org.jsoup.Jsoup.parse(reader.lines().collect(java.util.stream.Collectors.joining("\n")));
