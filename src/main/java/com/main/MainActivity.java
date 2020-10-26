55392df161e154a37bdf5cf114b6703b33845eca
chaowen guo <chaowen.guo1@gmail.com>
2020-10-25 21:49:55 -0500

    Update MainActivity.java

--- src/main/java/com/main/MainActivity.java
+++ src/main/java/com/main/MainActivity.java
@@ -13,9 +13,10 @@ public class MainActivity extends android.app.Activity
         final var button = new Button(this);
         try (final var reader = new java.io.BufferedReader(new java.io.InputStreamReader(super.getAssets().open("index.html"), java.nio.charset.StandardCharsets.UTF_8)))
         {
-            button.setText(reader.lines().collect(java.util.stream.Collectors.joining("\n")));
+            final var document = Jsoup.parse(reader.lines().collect(java.util.stream.Collectors.joining("\n")));
+            button.setText(document.body.html());
         }
-        //catch (Exception error) {}
+        catch (Exception error) {}
         body.addView(button);  
         super.setContentView(body, new ConstraintLayout.LayoutParams(ConstraintLayout.LayoutParams.MATCH_PARENT, ConstraintLayout.LayoutParams.MATCH_PARENT));
     }  
