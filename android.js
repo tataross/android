25279bfb7497c520387ff639d6e298ccc3229f33
chaowen guo <chaowen.guo1@gmail.com>
2022-03-26 07:29:07 -0500

    Create android.js

---
+++ android.js
@@ -0,0 +1,28 @@
+import webdriverio from 'webdriverio'
+
+const opts = {
+  path: '/wd/hub',
+  port: 4723,
+  capabilities: {
+    platformName: "Android",
+    platformVersion: "8",
+    deviceName: "Android Emulator",
+    app: "/path/to/the/downloaded/ApiDemos-debug.apk",
+    appPackage: "io.appium.android.apis",
+    appActivity: ".view.TextFields",
+    automationName: "UiAutomator2"
+  }
+};
+
+async function main () {
+  const client = await wdio.remote(opts);
+
+  const field = await client.$("android.widget.EditText");
+  await field.setValue("Hello World!");
+  const value = await field.getText();
+  assert.strictEqual(value,"Hello World!");
+
+  await client.deleteSession();
+}
+
+main()
