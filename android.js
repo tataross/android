6b5c63ac3e6997c63e5455781e1831467442781c
chaowen guo <chaowen.guo1@gmail.com>
2022-03-26 07:35:42 -0500

    Update android.js

--- android.js
+++ android.js
@@ -14,15 +14,8 @@ const opts = {
   }
 };
 
-async function main () {
-  const client = await wdio.remote(opts);
-
-  const field = await client.$("android.widget.EditText");
-  await field.setValue("Hello World!");
-  const value = await field.getText();
-  assert.strictEqual(value,"Hello World!");
-
-  await client.deleteSession();
-}
-
-main()
+const client = await webdriverio.remote(opts);
+const field = await client.$("android.widget.EditText");
+await field.setValue("Hello World!");
+const value = await field.getText();
+await client.deleteSession();
