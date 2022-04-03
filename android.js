5523ddb23023cf3bae757295076db7e5817dfd51
chaowen guo <chaowen.guo1@gmail.com>
2022-04-03 16:30:34 -0500

    Update android.js

--- android.js
+++ android.js
@@ -5,12 +5,15 @@ const opts = {
   hostname: '0.0.0.0',
   port: 4723,
   capabilities: {
-    platformName: "Android",
-    deviceName: "haha",
-    app: "ApiDemos-debug.apk",
-    appPackage: "io.appium.android.apis",
+    platformName: 'Android',
+    platformVersion: '11',
+    deviceName: 'Android Emulator',
+    automationName: 'UiAutomator2',
+    app: 'ApiDemos-debug.apk',
+    appPackage: 'io.appium.android.apis',
     appActivity: ".view.TextFields"
   }
 };
 
-webdriverio.startRecordingScreen()
+const client = await webdriverio.remote(opts);
+await client.deleteSession();
