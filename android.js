0d236a22e0c6c11248d866144ed3062b469844a6
chaowen guo <chaowen.guo1@gmail.com>
2022-04-04 23:17:28 -0500

    Update android.js

--- android.js
+++ android.js
@@ -15,7 +15,7 @@ const client = await webdriverio.remote(opts)
 await client.startRecordingScreen()
 for (const _ of globalThis.Array(10).keys())
 {
-    console.log(await client.getActiveElement())
+    console.log(await client.getWindowHandle())
     await new globalThis.Promise(_ => globalThis.setTimeout(_, 1000 * 10))
 }
 await client.installApp('cashmagnetNew.apk')
