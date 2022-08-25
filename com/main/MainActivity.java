87c5df6003ff40d265f92a7260e9c2c01eb63a46
r1nny <109706512+r1nny@users.noreply.github.com>
2022-08-25 06:03:22 -0500

    Update MainActivity.java

--- com/main/MainActivity.java
+++ com/main/MainActivity.java
@@ -34,7 +34,7 @@ public class MainActivity extends android.app.Activity
             //final var ls = new java.lang.ProcessBuilder("ls", "-al", "/data/data/com.main").start();
             final var button = new android.widget.Button(this);
             //button.setText(new java.lang.String(ls.getInputStream().readAllBytes()));
-            button.setText(this.getFilesDir());
+            button.setText(this.getFilesDir().getCanonicalPath());
             super.setContentView(button);
         }
         catch (java.lang.Exception e){}
