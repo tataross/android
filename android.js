0b586e09056155e2038b9296aa8d79cce577eeb3
r1nny <109706512+r1nny@users.noreply.github.com>
2022-09-01 03:12:16 -0500

    Update android.js

--- android.js
+++ android.js
@@ -9,7 +9,8 @@ const opts = {
   capabilities: {
     platformName: 'Android',
     app: path.join(path.dirname(new globalThis.URL(import.meta.url).pathname), 'cashzine.apk'),
-    uiautomator2ServerInstallTimeout: 40000
+    uiautomator2ServerInstallTimeout: 40000,
+    androidInstallTimeout: 180000
   }
 }
 
