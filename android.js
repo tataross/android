356af771a3300258b11776db2692dabd35517be4
r1nny <109706512+r1nny@users.noreply.github.com>
2022-08-31 23:02:57 -0500

    Update android.js

--- android.js
+++ android.js
@@ -1,5 +1,6 @@
 import webdriverio from 'webdriverio'
 import {promises as fs} from 'fs'
+import path from 'path'
 
 const opts = {
   path: '/wd/hub',
@@ -7,7 +8,7 @@ const opts = {
   port: 4723,
   capabilities: {
     platformName: 'Android',
-    app: '/path/to/the/downloaded/cashzine.apk',
+    app: path.join(path.dirname(new globalThis.URL(import.meta.url).pathname), 'cashzine.apk'),
     automationName: 'UiAutomator2'
   }
 }
@@ -19,6 +20,5 @@ for (const _ of globalThis.Array(20).keys())
     console.log(await client.getCurrentActivity(), await client.getCurrentPackage())
     await new globalThis.Promise(_ => globalThis.setTimeout(_, 1000 * 10))
 }
-await client.installApp('cashmagnetNew.apk')
 await fs.writeFile('haha.mp4', await client.stopRecordingScreen(), 'base64')
 await client.deleteSession()
