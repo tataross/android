3bad3e5858a1ade74f85010411adb854fcb60ed7
r1nny <109706512+r1nny@users.noreply.github.com>
2022-09-02 12:45:02 -0500

    Update android.js

--- android.js
+++ android.js
@@ -1,6 +1,7 @@
 import webdriverio from 'webdriverio'
 import {promises as fs} from 'fs'
 import path from 'path'
+import process from 'process'
 
 const opts =
 {
@@ -20,29 +21,20 @@ const opts =
 
 const client = await webdriverio.remote(opts)
 await client.startRecordingScreen()
-/*const tvLabel = await client.$$('id=com.sky.sea.cashzine:id/tv_label').at(0)
-await tvLabel.click()*/
-/*const tvFinish = await client.$('id=com.sky.sea.cashzine:id/tv_finshed')
-await tvFinish.click()
-const llAgree = await client.$('id=com.sky.sea.cashzine:id/ll_agree')
-await llAgree.click()
-const permissionAllowButton = await client.$('id=com.android.permissioncontroller:id/permission_allow_button')
-await permissionAllowButton.click()
-const ivHomeHome = await client.$('id=com.sky.sea.cashzine:id/iv_home_home')
-await ivHomeHome.click()
-const tvLoginNow = await client.$('id=com.sky.sea.cashzine:id/tv_login_now')
-await tvLoginNow.click()
-const tvGoToEmailLogin = await client.$('id=com.sky.sea.cashzine:id/tv_go_to_email_login')
-await tvGoToEmailLogin.click()
-const etPhoneEmail = await client.$('id=com.sky.sea.cashzine:id/et_phone_email')
-await etPhoneEmail.setValue('chaowen.guo1@gmail.com')
-const etPassword = await client.$('id=com.sky.sea.cashzine:id/et_password')*/
-for (const _ of globalThis.Array(20).keys())
+for (const _ of globalThis.Array(10).keys())
 {
     console.log(await client.getCurrentActivity(), await client.getCurrentPackage())
     await new globalThis.Promise(_ => globalThis.setTimeout(_, 1000 * 10))
 }
 const tvLabel = await client.$$('id=com.sky.sea.cashzine:id/tv_label').at(0)
 await tvLabel.click()
+await client.$('id=com.sky.sea.cashzine:id/tv_finshed').click()
+await client.$('id=com.sky.sea.cashzine:id/ll_agree').click()
+await client.$('id=com.android.permissioncontroller:id/permission_allow_button').click()
+await client.$('id=com.sky.sea.cashzine:id/iv_home_home').click()
+await client.$('id=com.sky.sea.cashzine:id/tv_login_now').click()
+await client.$('id=com.sky.sea.cashzine:id/tv_go_to_email_login').click()
+await client.$('id=com.sky.sea.cashzine:id/et_phone_email').setValue('chaowen.guo1@gmail.com')
+await client.$('id=com.sky.sea.cashzine:id/et_password').setValue(process.argv.at(2))
 await fs.writeFile('haha.mp4', await client.stopRecordingScreen(), 'base64')
 await client.deleteSession()
