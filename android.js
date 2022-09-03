d702b1ba2f8fcfd97645e4b484a8ba47b628c6ea
r1nny <109706512+r1nny@users.noreply.github.com>
2022-09-03 09:13:32 -0500

    Update android.js

--- android.js
+++ android.js
@@ -39,6 +39,7 @@ await client.$('id=com.sky.sea.cashzine:id/et_password').setValue(process.argv.a
 await client.$('id=com.sky.sea.cashzine:id/tv_login').click()
 await client.$('id=com.sky.sea.cashzine:id/iv_home_work').click()
 await client.$('id=com.sky.sea.cashzine:id/iv_article_icon_big').click()
+console.log(await client.getContexts())
 await new globalThis.Promise(_ => globalThis.setTimeout(_, 1000 * 30))
 console.log(await client.getWindowRect())
 
