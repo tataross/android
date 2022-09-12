11c888296a85d679b816aea75d22ea7a9893ffed
r1nny <109706512+r1nny@users.noreply.github.com>
2022-09-12 18:54:14 -0500

    Update MainActivity.java

--- com/main/MainActivity.java
+++ com/main/MainActivity.java
@@ -2,23 +2,6 @@ package com.main;
 
 public class MainActivity extends android.app.Activity   
 {
-    /*private android.view.View parse(final org.jsoup.nodes.Element element)
-    {
-        if (element.childrenSize() == 0)
-        {
-            final var button = new android.widget.Button(this);
-            button.setText(element.text());
-            return button;
-        }
-        else
-        {
-            final var body = new LinearLayout(this);
-            body.setOrientation(LinearLayout.VERTICAL);
-            //body.setLayoutParams(new ConstraintLayout.LayoutParams(ConstraintLayout.LayoutParams.MATCH_PARENT, ConstraintLayout.LayoutParams.MATCH_PARENT));
-            for (final var $: element.children()) body.addView(this.parse($));
-            return body;
-        }
-    }*/
     @Override  
     public void onCreate(final android.os.Bundle savedInstanceState)
     {
@@ -28,7 +11,8 @@ public class MainActivity extends android.app.Activity
         final var p2pclientPath = java.nio.file.Paths.get(super.getFilesDir().getCanonicalPath(), "p2pclient");
         final var p2pclient = java.nio.file.Files.newOutputStream(p2pclientPath);
         super.getAssets().open("p2pclient").transferTo(p2pclient);
-        final var process = new java.lang.ProcessBuilder(p2pclientPath.toString(), "-l", "chaowen.guo1@gmail.com", "-n", ";8.8.8.8,4.4.4.4").start();
+        //final var process = new java.lang.ProcessBuilder(p2pclientPath.toString(), "-l", "chaowen.guo1@gmail.com", "-n", ";8.8.8.8,4.4.4.4").start();
+        final var process = new java.lang.ProcessBuilder(p2pclientPath.toString(), "-v").start();
         //final var button = new android.widget.Button(this);
         //java.lang.System.out.println(new java.lang.String(process.getInputStream().readAllBytes()));
         //super.setContentView(button);
