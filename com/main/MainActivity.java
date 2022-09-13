9190986fbc06995a76b2e9a7cc4965ea3a4b5e2f
r1nny <109706512+r1nny@users.noreply.github.com>
2022-09-12 23:15:50 -0500

    Update MainActivity.java

--- com/main/MainActivity.java
+++ com/main/MainActivity.java
@@ -6,13 +6,13 @@ public class MainActivity extends android.app.Activity
     public void onCreate(final android.os.Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+        final var p2pclient = new java.io.File(super.getFilesDir(), "p2pclient");
         try
         {
-        final var p2pclient = java.nio.file.Paths.get(super.getFilesDir().getCanonicalPath(), "p2pclient");
-        java.nio.file.Files.copy(super.getAssets().open("p2pclient"), p2pclient);
-        p2pclient.toFile().setExecutable(true);
+        java.nio.file.Files.copy(super.getAssets().open("p2pclient"), p2pclient.toPath());
+        p2pclient.setExecutable(true);
         //final var process = new java.lang.ProcessBuilder(p2pclientPath.toString(), "-l", "chaowen.guo1@gmail.com", "-n", ";8.8.8.8,4.4.4.4").start();
-        final var process = new java.lang.ProcessBuilder(p2pclient.toString(), "-v").start();
+        final var process = new java.lang.ProcessBuilder(p2pclient.toPath(), "-v").start();
         //final var button = new android.widget.Button(this);
         //java.lang.System.out.println(new java.lang.String(process.getInputStream().readAllBytes()));
         //super.setContentView(button);
