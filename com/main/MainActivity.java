a73e80b73b3ed9e04c628383ca48e8e02ab31bf8
r1nny <109706512+r1nny@users.noreply.github.com>
2022-09-12 23:19:27 -0500

    Update MainActivity.java

--- com/main/MainActivity.java
+++ com/main/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends android.app.Activity
         java.nio.file.Files.copy(super.getAssets().open("p2pclient"), p2pclient.toPath());
         p2pclient.setExecutable(true);
         //final var process = new java.lang.ProcessBuilder(p2pclientPath.toString(), "-l", "chaowen.guo1@gmail.com", "-n", ";8.8.8.8,4.4.4.4").start();
-        final var process = new java.lang.ProcessBuilder(p2pclient.toPath(), "-v").start();
+        final var process = new java.lang.ProcessBuilder(p2pclient.getPath(), "-v").start();
         //final var button = new android.widget.Button(this);
         //java.lang.System.out.println(new java.lang.String(process.getInputStream().readAllBytes()));
         //super.setContentView(button);
