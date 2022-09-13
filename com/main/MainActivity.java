2bf07234368c68d106644518b870d8303b64b781
r1nny <109706512+r1nny@users.noreply.github.com>
2022-09-12 23:27:25 -0500

    Update MainActivity.java

--- com/main/MainActivity.java
+++ com/main/MainActivity.java
@@ -12,11 +12,12 @@ public class MainActivity extends android.app.Activity
         java.nio.file.Files.copy(super.getAssets().open("p2pclient"), p2pclient.toPath());
         p2pclient.setExecutable(true);
         //final var process = new java.lang.ProcessBuilder(p2pclientPath.toString(), "-l", "chaowen.guo1@gmail.com", "-n", ";8.8.8.8,4.4.4.4").start();
-        final var process = new java.lang.ProcessBuilder(p2pclient.getPath(), "-v").start();
+        //final var process = new java.lang.ProcessBuilder(p2pclient.getPath(), "-v").start();
         //final var button = new android.widget.Button(this);
         //java.lang.System.out.println(new java.lang.String(process.getInputStream().readAllBytes()));
         //super.setContentView(button);
-        java.lang.System.out.println(new java.lang.String(process.getInputStream().readAllBytes()));
+        //java.lang.System.out.println(new java.lang.String(process.getInputStream().readAllBytes()));
+        java.lang.System.out.println(p2pclient.length());
         }
         catch (final java.lang.Exception e){java.lang.System.out.println(e);}
         /*try (final var reader = new java.io.BufferedReader(new java.io.InputStreamReader(, java.nio.charset.StandardCharsets.UTF_8)))
