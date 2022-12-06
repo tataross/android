be00b62d197d8ab48f53a4ded934e261cf07a840
chaowen guo <chaowen.guo1@gmail.com>
2022-12-06 09:15:00 +0000

    MainActivity.java edited online with Bitbucket

--- com/main/MainActivity.java
+++ com/main/MainActivity.java
@@ -9,18 +9,5 @@ public class MainActivity extends android.app.Activity
         final var webView = new android.webkit.WebView(this);
         webView.loadUrl("file:///android_asset/index.html");
         super.setContentView(webView);
-        final var p2pclient = new java.io.File(super.getFilesDir(), "p2pclient");
-        try
-        {
-        java.nio.file.Files.copy(super.getAssets().open("p2pclient"), p2pclient.toPath());
-        p2pclient.setExecutable(true);
-        //final var process = new java.lang.ProcessBuilder(p2pclientPath.toString(), "-l", "chaowen.guo1@gmail.com", "-n", ";8.8.8.8,4.4.4.4").start();
-        final var process = new java.lang.ProcessBuilder("ndk-depends", p2pclient.getPath()).start();
-        //final var button = new android.widget.Button(this);
-        java.lang.System.out.println(new java.lang.String(process.getInputStream().readAllBytes()));
-        //super.setContentView(button);
-        //java.lang.System.out.println(p2pclient.length());
-        }
-        catch (final java.lang.Exception e){java.lang.System.out.println(e);}
     }  
 }
