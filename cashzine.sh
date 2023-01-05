2786e7930bf66312a08631729cecfdb3c1fc76e1
tataross <119935693+tataross@users.noreply.github.com>
2023-01-04 22:52:36 -0800

    Update cashzine.sh

--- cashzine.sh
+++ cashzine.sh
@@ -33,9 +33,9 @@ sdk/platform-tools/adb install cashzine.apk
 #adb exec-out dumpsys activity | awk /mCurrentFocus/
 sdk/platform-tools/adb exec-out 'am start -n com.termux/com.termux.app.TermuxActivity
 sleep 10
-/data/data/com.termux/files/usr/bin/gawk -v RS=\\n{10} {print\ gensub\(/\\xb4\\x00\\x00\\x00/\,\"\\xff\\xff\\xff\\xff\"\,20\)} /system/bin/screenrecord | /data/data/com.termux/files/usr/bin/head -c -1 > /data/local/tmp/screenrecord
-mv /data/local/tmp/screenrecord /system/bin
-/system/bin/linker64 /system/bin/screenrecord --output-format=h264 - &
+#/data/data/com.termux/files/usr/bin/gawk -v RS=\\n{10} {print\ gensub\(/\\xb4\\x00\\x00\\x00/\,\"\\xff\\xff\\xff\\xff\"\,20\)} /system/bin/screenrecord | /data/data/com.termux/files/usr/bin/head -c -1 > /data/local/tmp/screenrecord
+#mv /data/local/tmp/screenrecord /system/bin
+/system/bin/screenrecord --output-format=h264 - &
 tap()
 {
     sleep 5
@@ -59,27 +59,4 @@ tap item_container
 array=($(wm size | awk {sub\(/x/\,\"\ \"\,\$NF\)\;print\$NF}))
 halfWidth=$((${array[0]} / 2))
 height=${array[1]}
-input tap $halfWidth $((height / 2))
-while true
-do
-    uiautomator dump /data/local/tmp/ui.xml
-    icon=($(awk -v RS=\> -F= /icon/{gsub\(/[][\,\"]/\,\"\ \"\,\$NF\)\;print\$NF} /data/local/tmp/ui.xml))
-    for i in $(seq 0 1)
-    do
-        for j in $(seq 0 1)
-        do
-	    sleep 10
-            input swipe $halfWidth $(($((height / 10)) * 9)) $halfWidth $((height / 10))
-        done
-        for j in $(seq 0 1)
-        do
-	    sleep 10
-            input swipe $halfWidth $((height / 10)) $halfWidth $(($((height / 10)) * 9))
-        done
-    done
-    sleep 5
-    input tap $(($((${icon[0]} + ${icon[2]})) / 2)) $(($((${icon[1]} + ${icon[3]})) / 2))
-    sleep 5
-    input keyevent 4
-    tap item_container
-done' > cashzine.mp4
+input tap $halfWidth $((height / 2))' > cashzine.mp4
