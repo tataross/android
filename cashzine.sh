570bbad578908bd746a091283d5e327b805e3dfc
tataross <119935693+tataross@users.noreply.github.com>
2023-01-05 00:05:34 -0800

    Update cashzine.sh

--- cashzine.sh
+++ cashzine.sh
@@ -35,7 +35,6 @@ sdk/platform-tools/adb exec-out 'am start -n com.termux/com.termux.app.TermuxAct
 sleep 10
 /data/data/com.termux/files/usr/bin/gawk -v RS=\\n{10} {print\ gensub\(/\\xb4\\x00\\x00\\x00/\,\"\\xff\\xff\\xff\\xff\"\,20\)} /system/bin/screenrecord | /data/data/com.termux/files/usr/bin/head -c -1 > /data/local/tmp/screenrecord
 mv /data/local/tmp/screenrecord /system/bin
-/system/bin/screenrecord --output-format=h264 - &
 tap()
 {
     sleep 1m
@@ -43,6 +42,7 @@ tap()
     array=($(awk -v RS=\> -F= /$1/{gsub\(/[][\,\"]/\,\"\ \"\,\$NF\)\;print\$NF} /data/local/tmp/ui.xml))
     input tap $(($((${array[0]} + ${array[2]})) / 2)) $(($((${array[1]} + ${array[3]})) / 2))
 }
+/system/bin/screenrecord --output-format=h264 - &
 
 am start -n com.sky.sea.cashzine/com.sky.sea.home.main.MainActivity
 tap ll_home_home
@@ -59,4 +59,27 @@ tap item_container
 array=($(wm size | awk {sub\(/x/\,\"\ \"\,\$NF\)\;print\$NF}))
 halfWidth=$((${array[0]} / 2))
 height=${array[1]}
-input tap $halfWidth $((height / 2))' > cashzine.mp4
+input tap $halfWidth $((height / 2))
+for k in $(seq 0 2)
+do
+    uiautomator dump /data/local/tmp/ui.xml
+    icon=($(awk -v RS=\> -F= /icon/{gsub\(/[][\,\"]/\,\"\ \"\,\$NF\)\;print\$NF} /data/local/tmp/ui.xml))
+    for i in $(seq 0 1)
+    do
+        for j in $(seq 0 1)
+        do
+	    sleep 10
+            input swipe $halfWidth $(($((height / 10)) * 9)) $halfWidth $((height / 10))
+        done
+        for j in $(seq 0 1)
+        do
+	    sleep 10
+            input swipe $halfWidth $((height / 10)) $halfWidth $(($((height / 10)) * 9))
+        done
+    done
+    sleep 5
+    input tap $(($((${icon[0]} + ${icon[2]})) / 2)) $(($((${icon[1]} + ${icon[3]})) / 2))
+    sleep 5
+    input keyevent 4
+    tap item_container
+done' > cashzine.mp4
